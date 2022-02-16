644e4c028bf7039340a0f5fe948c84f941e1eaa6
leiu <leiuyi@outlook.com>
2022-02-17 00:37:30 +0800

    添加项目文件。

---
+++ Form.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.MenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Location = new System.Drawing.Point(12, 31);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(776, 407);
+            this.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox.TabIndex = 0;
+            this.PictureBox.TabStop = false;
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(800, 25);
+            this.MenuStrip.TabIndex = 1;
+            this.MenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.openFolderToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.OpenFileToolStripMenuItem_Click);
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.O)));
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.openFolderToolStripMenuItem.Text = "Open Folder";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem1});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.aboutToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(111, 22);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.AboutToolStripMenuItem1_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.MenuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.MenuStrip;
+            this.Name = "Form";
+            this.Text = "Show Photo";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+    }
+}
+
---
+++ Form.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private int index, count = 0;
+        private ArrayList images = new ArrayList();
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            string command = Environment.CommandLine;
+            string[] para = command.Split('\"');
+            if (para.Length > 3)
+            {
+                try
+                {
+                    PictureBox.Image = Image.FromFile(para[3]);
+                    LoadDirectory(para[3]);
+                }
+                catch (Exception)
+                {
+                   // nothing...
+                }
+                
+            }
+
+            PictureBox.MouseWheel += new MouseEventHandler(PictureBox1_MouseWheel);
+        }
+
+        private void PictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (PictureBox.Image == null || Width + e.Delta > 1920 || Height + e.Delta > 1080 || Width + e.Delta < 500 || Height + e.Delta < 450)
+            {
+                return;
+            }
+
+            PictureBox.Width += e.Delta;
+            PictureBox.Height += e.Delta;
+            Width += e.Delta;
+            Height += e.Delta;
+        }
+
+        private void OpenFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog(); 
+            ofd.Multiselect = false;
+            ofd.Title = "Select the Image";
+            ofd.Filter ="Image Files(*.jpeg;*.jpg;*.png)|*.jpeg;*.jpg;*.png";
+            DialogResult dialogResult = ofd.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                string path = ofd.FileName;
+                PictureBox.Image = Image.FromFile(path);
+                LoadDirectory(ofd.FileName);
+            }
+            else if (dialogResult == DialogResult.Cancel)
+            {
+                // nothing...
+            }
+            else
+            {
+                MessageBox.Show("Please select an image file!");
+
+            }
+
+        }
+
+        private void LoadDirectory(string fileName)
+        {
+            DirectoryInfo rootDir = new DirectoryInfo(fileName.Remove(fileName.LastIndexOf("\\")));
+
+            foreach (var item in rootDir.GetFiles())
+            {
+                try
+                {
+                    Image.FromFile(item.FullName);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+
+                images.Add(item.FullName);
+            }
+
+            images.Sort(new ImageComparer());
+            index = images.IndexOf(fileName);
+            count = images.Count;   
+
+        }
+
+        private void AboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("\t Show Photo\t \n\r\n\r\t Version: 1.0.0\t", "About");
+
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void Form_KeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyData == Keys.Left && index > 0)
+            {
+                PictureBox.Image = Image.FromFile(images[index - 1].ToString());
+                index--;
+            }
+            else if (e.KeyData == Keys.Right && index < count - 1)
+            {
+                PictureBox.Image = Image.FromFile(images[index + 1].ToString());
+                index++;
+
+            }
+            else
+            {
+                // nothing...
+            }
+        }
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (keyData == Keys.Left || keyData == Keys.Right)
+            {
+                return false;
+            }
+            else
+            {
+                return base.ProcessDialogKey(keyData);
+            }
+        }
+
+        class ImageComparer : IComparer
+        {
+            public int Compare(object x, object y)
+            {
+                string sx = x.ToString();
+                string sy = y.ToString();
+                return sx.CompareTo(sy);
+            }
+        }
+
+    }
+}
